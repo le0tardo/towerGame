8b22e46579a57533b924564316e5b18e5578eb79
Leo Lutecki <leo.lutecki@learnet.se>
2025-04-08 10:47:07 +0200

    next level button

---
+++ towerOffence/Assets/Scripts/GUI/NextSceneButton.cs
@@ -0,0 +1,19 @@
+ using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneButton : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more levels yet lol!");
+        }
+    }
+}
